c0d42be51ef67e834c7cefe257c3eb5f318a29d1
Rahul Gopinath <rahul@gopinath.org>
2017-12-29 02:20:06 -0800

    update

--- plot.py
+++ plot.py
@@ -3,30 +3,41 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import sys
 import os
+import io
 
 import ggplot as gg
 import pandas as pd
 
 
-def plot(mydata, opts, lim):
+def plot(mydata, opts):
     # number of mutants killed by exactly 0 tests
-    nd = mydata[mydata.exactly == 0]
-    total = 10000
-    title = opts['title'] + (' ND=%d/%d (Mu: %3.1f%%)' % (len(nd), total, (1 - len(nd) / total)*100.0 ))
+    nd = sum(mydata[mydata.ntests == 0].exactly)
+    d = sum(mydata[mydata.ntests != 0].exactly)
+    total = nd + d
+    print("Not detected = ", nd, "/", total)
+    title = opts['title'] + (' ND=%d/%d (Mu: %3.1f%%)' % (nd, total, (1 - nd/ total)*100.0 ))
     p = gg.ggplot(gg.aes(x=opts['x'], y=opts['y']), data=mydata) + gg.geom_point() +\
-            gg.xlab(opts['x']) + gg.ylab(opts['y']) + gg.ggtitle(title)  + \
-       gg.xlim(0,lim)
+            gg.xlab(opts['x']) + gg.ylab(opts['y']) + gg.ggtitle(title)  #+ \
+    #   gg.xlim(0,lim)
 
     p.save(opts['file'])
 
-def do_statistics(data, fn):
-    plot(data, {'x':'ntests', 'y':'atleast', 'title':fn.replace('_', ' '), 'file':fn + '-atleast.png'}, 1000)
-    plot(data, {'x':'ntests', 'y':'exactly', 'title':fn.replace('_', ' '), 'file':fn + '-exact.png'}, 1000)
-    plot(data, {'x':'ntests', 'y':'atmost', 'title':fn.replace('_', ' '), 'file':fn + '-atmost.png'}, 1000)
+def sanitize(s):
+    return s.replace('_', ' ').replace('kills.csv', '')
+
+def plotit(data, fn):
+    print('exact')
+    plot(data, {'x':'ntests', 'y':'exactly', 'title':sanitize(fn), 'file':fn + '-exact.png'})
+    print('atleast')
+    plot(data, {'x':'ntests', 'y':'atleast', 'title':sanitize(fn), 'file':fn + '-atleast.png'})
+    print('atmost')
+    plot(data, {'x':'ntests', 'y':'atmost', 'title':sanitize(fn), 'file':fn + '-atmost.png'})
     print('done', file=sys.stderr)
 
 def main(args):
     fn = args[0]
-    do_statistics(pd.read_csv(fn), fn)
+    v = open(fn).read()
+    d =  pd.read_csv(io.StringIO(v.replace(' ', '')))
+    plotit(d, fn)
 
 main(sys.argv[1:])
